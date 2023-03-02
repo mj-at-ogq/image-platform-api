32eb87258f51f926625a4e2f2c2b7eb0b0e4eea1
MJbae <gentlemmj@gmail.com>
2023-03-02 14:55:26 +0900

    refactor: 공개 메소드의 가독성 향상을 위해 로컬 함수를 비공개 메소드로 수정

--- gateway/src/main/kotlin/me/ogq/ocp/sample/gateway/elasticsearch/SearchEngineImpl.kt
+++ gateway/src/main/kotlin/me/ogq/ocp/sample/gateway/elasticsearch/SearchEngineImpl.kt
@@ -2,9 +2,9 @@
 
 package me.ogq.ocp.sample.gateway.elasticsearch
 
-import me.ogq.ocp.sample.model.SearchEngine
+import me.ogq.ocp.sample.model.elasticsearch.SearchEngine
 import me.ogq.ocp.sample.model.image.Image
-import me.ogq.ocp.sample.model.image.ImageData
+import me.ogq.ocp.sample.model.event.ImageData
 import me.ogq.ocp.sample.model.image.TagStringSetConverter
 import me.ogq.ocp.sample.model.publicityright.Market
 import me.ogq.ocp.sample.model.publicityright.PublicityRight
@@ -27,86 +27,86 @@ class SearchEngineImpl(
     private val tagConverter: TagStringSetConverter
 ) : SearchEngine {
     override fun save(image: Image) {
-        fun createImageDoc(image: Image): Map<String, Any?> {
-            return mapOf(
-                "image_id" to image.id.toString(),
-                "title" to image.title,
-                "description" to image.description,
-                "imagePath" to image.file.path.toString(),
-                "tags" to tagConverter.convertToDatabaseColumn(image.tags),
-                "creator_id" to image.authorId.toString(),
-                "publicity_id" to image.publicityRightId.toString(),
-            )
-        }
+        val indexRequest = createIndexRequest(image)
 
-        val doc: Map<String, Any?> = createImageDoc(image)
-
-        client.index(
-            IndexRequest(indexName).id(doc["image_id"] as String).source(doc),
-            RequestOptions.DEFAULT
-        )
+        client.index(indexRequest, RequestOptions.DEFAULT)
     }
 
     override fun searchWith(market: Market, query: String, page: Int, pageSize: Int): List<ImageData> {
-        fun generateSearchRequest(publicityRight: PublicityRight?, query: String): SearchRequest {
-            val queryBuilder = QueryBuilders.boolQuery()
-
-            queryBuilder.must(
-                QueryBuilders.queryStringQuery("*$query*")
-                    .field("title")
-                    .field("tags")
-                    .lenient(true)
-                    .defaultOperator(Operator.OR)
-            )
-
-            if (publicityRight != null) {
-                queryBuilder.should(QueryBuilders.termQuery("publicity_id", publicityRight.id.toString()))
-            } else {
-                queryBuilder.should(QueryBuilders.boolQuery().mustNot(QueryBuilders.existsQuery("publicity_id")))
-            }
-
-            val sourceBuilder = SearchSourceBuilder()
-            sourceBuilder.query(queryBuilder)
-            sourceBuilder.from(page * pageSize)
-            sourceBuilder.size(pageSize)
-
-            return SearchRequest(indexName).source(sourceBuilder)
-        }
-
-        fun parseFrom(searchResponse: SearchResponse): List<ImageData> {
-            val hits = searchResponse.hits.hits
-            val images = mutableListOf<ImageData>()
-
-            for (hit in hits) {
-                val sourceMap = hit.sourceAsMap
-                val id = sourceMap["image_id"]?.toString()?.toLong()
-                val title = sourceMap["title"] as String
-                val description = sourceMap["description"] as String?
-                val tags = tagConverter.convertToEntityAttribute(sourceMap["tags"] as String)
-                val creatorId = sourceMap["creator_id"]?.toString()?.toLong()
-                val imagePath = sourceMap["imagePath"] as String
-                val publicityRightId = if (sourceMap["publicity_id"] == "null") null else sourceMap["publicity_id"]?.toString()?.toLong()
-
-                val image = ImageData(
-                    id = id,
-                    title = title,
-                    description = description,
-                    tags = tags,
-                    publicityRightId = publicityRightId,
-                    authorId = creatorId ?: throw IllegalArgumentException("creatorId should not be null"),
-                    imagePath = imagePath
-                )
-
-                images.add(image)
-            }
-
-            return images
-        }
-
-        val searchRequest = generateSearchRequest(market.publicityRight, query)
+        val searchRequest = createSearchRequest(market.publicityRight, query, page, pageSize)
 
         val searchResponse = client.search(searchRequest, RequestOptions.DEFAULT)
 
-        return parseFrom(searchResponse)
+        return parseImageListFrom(searchResponse)
+    }
+
+    private fun createIndexRequest(image: Image): IndexRequest {
+        val doc: Map<String, Any?> = mapOf(
+            "image_id" to image.id.toString(),
+            "title" to image.title,
+            "description" to image.description,
+            "imagePath" to image.file.path.toString(),
+            "tags" to tagConverter.convertToDatabaseColumn(image.tags),
+            "creator_id" to image.authorId.toString(),
+            "publicity_id" to image.publicityRightId.toString(),
+        )
+
+        return IndexRequest(indexName).id(doc["image_id"] as String).source(doc)
+    }
+
+    private fun createSearchRequest(publicityRight: PublicityRight?, query: String, page: Int, pageSize: Int): SearchRequest {
+        val queryBuilder = QueryBuilders.boolQuery()
+
+        queryBuilder.must(
+            QueryBuilders.queryStringQuery("*$query*")
+                .field("title")
+                .field("tags")
+                .lenient(true)
+                .defaultOperator(Operator.OR)
+        )
+
+        if (publicityRight != null) {
+            queryBuilder.should(QueryBuilders.termQuery("publicity_id", publicityRight.id.toString()))
+        } else {
+            queryBuilder.should(QueryBuilders.boolQuery().mustNot(QueryBuilders.existsQuery("publicity_id")))
+        }
+
+        val sourceBuilder = SearchSourceBuilder()
+        sourceBuilder.query(queryBuilder)
+        sourceBuilder.from(page * pageSize)
+        sourceBuilder.size(pageSize)
+
+        return SearchRequest(indexName).source(sourceBuilder)
+    }
+
+    private fun parseImageListFrom(searchResponse: SearchResponse): List<ImageData> {
+        val hits = searchResponse.hits.hits
+        val images = mutableListOf<ImageData>()
+
+        for (hit in hits) {
+            val sourceMap = hit.sourceAsMap
+            val id = sourceMap["image_id"]?.toString()?.toLong()
+            val title = sourceMap["title"] as String
+            val description = sourceMap["description"] as String?
+            val tags = tagConverter.convertToEntityAttribute(sourceMap["tags"] as String)
+            val creatorId = sourceMap["creator_id"]?.toString()?.toLong()
+            val imagePath = sourceMap["imagePath"] as String
+            val publicityRightId =
+                if (sourceMap["publicity_id"] == "null") null else sourceMap["publicity_id"]?.toString()?.toLong()
+
+            val image = ImageData(
+                id = id,
+                title = title,
+                description = description,
+                tags = tags,
+                publicityRightId = publicityRightId,
+                authorId = creatorId ?: throw IllegalArgumentException("creatorId should not be null"),
+                imagePath = imagePath
+            )
+
+            images.add(image)
+        }
+
+        return images
     }
 }
