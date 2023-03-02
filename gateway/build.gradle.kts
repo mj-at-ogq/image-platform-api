a7db3cc8e44885160cc9e4709bd3ca340e6131df
MJbae <gentlemmj@gmail.com>
2023-03-02 10:54:47 +0900

    fix: 의존성 적절하게 불러오기 위해 dependencies 순서 변경

--- gateway/build.gradle.kts
+++ gateway/build.gradle.kts
@@ -11,7 +11,7 @@ jar.enabled = true
 jar.archiveClassifier.convention("") // spring boot 2.5 부터 일반 jar 은 -plain.jar 로 변경됨. 이로 인하여 소스가 인포트되지 않음.
 
 dependencies {
-    implementation(project(":model"))
     implementation("org.elasticsearch.client:elasticsearch-rest-high-level-client")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation(project(":model"))
 }
